c2226740f01a81ca3d44bdba19c9942163dfa0f5
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-01-26 22:22:13 +1300

    add insert query

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,6 +1,10 @@
 import { connectionString } from '../config'
 import sql from 'mssql'
 
+interface Person{
+  firstName:string,
+  lastName:string
+}
 
 export async function connect() {
   try {
@@ -14,8 +18,19 @@ export async function connect() {
 export async function getAllPersons(){
   try{
     const result = await sql.query("SELECT * FROM Person")
-    return result
+    return result.recordset
   }catch(e){
     console.error("Failed getting all person record",e)
+  }
+}
+
+export async function insertPerson(data:Person){
+  try{
+    const result = await sql.query`
+    INSERT INTO Person (firstName,lastName)
+    VALUES (${data.firstName}, ${data.lastName})`
+    return result.rowsAffected
+  }catch(e){
+    console.error("Failed inserting person",e)
   }
 }
--- server/router/person.ts
+++ server/router/person.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllPersons } from '../database/connection'
+import { getAllPersons, insertPerson } from '../database/connection'
 const router = express.Router()
 router.use(express.json())
 //api/v1/persons
@@ -11,6 +11,19 @@ router.get('/',async(req,res)=>{
     res.json(response)
   }catch(e){
     res.status(500).json("failed getting all persons")
+  }
+})
+router.post('/', async(req,res)=>{
+  try{
+    const person = req.body
+    const result = {
+      firstName:person.firstName,
+      lastName:person.lastName
+    }
+    const response = await insertPerson(result)
+    res.status(200).send(response)
+  }catch(e){
+    res.status(500).json("Internal Server Error")
   }
 })
 export default router
