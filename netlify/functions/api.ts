4f2d1dd2aae15cf9a12cd3b2f7d7390321f23fa2
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-01-27 17:38:35 +1300

    add process.env.PORT

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -13,7 +13,7 @@ const corsOptions = {
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 
 app.listen(port, () => {
   console.log('server is running on port', port)
