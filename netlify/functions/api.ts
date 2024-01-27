dd8cd6b59e6acfdae0301a6e595cb04c246d2d47
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-01-27 17:25:35 +1300

    add cors

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -2,8 +2,14 @@ import express from 'express'
 import {connect} from '../../server/database/connection.ts'
 import person from '../../server/router/person.ts'
 import serverless from "serverless-http";
+import cors from 'cors'
 //connect to database
-
+const corsOptions = {
+  origin:'https://main--chimerical-bombolone-a885d6.netlify.app/api/.netlify/functions/api',
+  methods:'GET',
+  credentials:true,
+  optionsSuccessStatus:204
+}
 
 
 const app = express()
@@ -12,7 +18,7 @@ const port = 3000
 app.listen(port, () => {
   console.log('server is running on port', port)
 })
-
+app.use(cors(corsOptions))
 app.use('/api/',person)
 
 export const handler = serverless(app)
