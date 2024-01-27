749649319101c16134ca793604e7d9f3f03868e9
Anthony Santiago <anthony.santiago58@yahoo.com>
2024-01-27 18:18:44 +1300

    test

--- server/router/person.ts
+++ server/router/person.ts
@@ -9,7 +9,7 @@ router.get('/.netlify/functions/api',async(req,res)=>{
   try{
     
     const response = await getAllPersons()
-    res.send(response)
+    res.send(`<h1>${response}</h1>`)
   }catch(e){
     res.status(500).json("failed getting all persons")
   }
